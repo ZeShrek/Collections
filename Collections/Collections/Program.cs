a704b0ebfb7ec4ea6bef763c16974d018e469517
Vitor Moura <zeshrek1996@gmail.com>
2020-11-07 13:15:27 +0000

    Initial commit

---
+++ Collections/Collections/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfTheWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            DisplayDays(daysOfTheWeek);
+
+            SpecificDayOfTheWeek(daysOfTheWeek);
+        }
+
+        static void DisplayDays(string[] daysOfTheWeek)
+        {
+            foreach (var day in daysOfTheWeek)
+            {
+                Console.WriteLine(day);
+            }
+        }
+
+        private static void SpecificDayOfTheWeek(string[] daysOfTheWeek)
+        {
+            Console.WriteLine("From 1 to 7 which day u want to display? ");
+            int dayNum = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+            Console.WriteLine($"That day is {daysOfTheWeek[dayNum - 1]}");
+        }
+    }
+}
