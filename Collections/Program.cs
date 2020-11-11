e98872d9314e9989de2c9dab16a221d1eb0ce84a
Vitor Moura <zeshrek1996@gmail.com>
2020-11-11 00:04:41 +0000

    for loop

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -13,9 +13,9 @@ namespace Collections
 
             // WithArrays(reader);
 
-            //WithList(reader);
+            WithList(reader);
 
-            WithDictionary(reader);
+            //WithDictionary(reader);
         }
 
         public static void WithArrays(CsvReader reader)
@@ -49,14 +49,69 @@ namespace Collections
             //removing from the list
             countries.RemoveAt(tanzaniaIndex);
 
-            Console.WriteLine($"Top {countries.Count}º most populated:");
-            Console.WriteLine();
+            //Console.WriteLine($"Top {countries.Count}º most populated:");
+            //Console.WriteLine();
 
-            foreach (var country in countries)
+            /*foreach (var country in countries)
             {
                 Console.WriteLine($"{(country.Name).PadRight(15)} : {FormatPopulation.PopulationFormat(country.Population).PadLeft(15)}");
+            }*/
+
+            Console.WriteLine("Enter the no. of countries to display: ");
+            bool inputIsInt = int.TryParse(Console.ReadLine(), out int userInput);
+            if (!inputIsInt || userInput <= 0)
+            {
+                Console.Write("You must type a positive integer. Exiting...");
+                return;
             }
+
+            int maxToDisplay = Math.Min(userInput, countries.Count);
+
+            //fisrt to the last
+            //DisplayFirstToLast(countries, maxToDisplay);
+
+
+            //last to the first
+            DisplayLastToFirst(countries, maxToDisplay);
+
             Console.WriteLine();
+        }
+
+        private static void DisplayLastToFirst(List<Country> countries, int maxToDisplay)
+        {
+            for (int i = countries.Count - 1; i >= 0; i--)
+            {
+                int displayIndex = countries.Count - 1 - i;
+                if (displayIndex > 0 && (displayIndex % maxToDisplay == 0))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Hit return to continue, anything else to quit: ");
+                    if (Console.ReadLine() != "")
+                        break;
+                }
+
+                Country country = countries[i];
+                Console.WriteLine(
+                    $"{i + 1} : {(country.Name).PadRight(15)} : {FormatPopulation.PopulationFormat(country.Population).PadLeft(15)}");
+            }
+        }
+
+        private static void DisplayFirstToLast(List<Country> countries, int maxToDisplay)
+        {
+            //first to the last
+            for (int i = 0; i < countries.Count; i++)
+            {
+                if (i > 0 && (i % maxToDisplay == 0))
+                {
+                    Console.WriteLine("Hit return to continue, anything else to quit: ");
+                    if (Console.ReadLine() != "")
+                        break;
+                }
+
+                Country country = countries[i];
+                Console.WriteLine(
+                    $"{i + 1} : {(country.Name).PadRight(15)} : {FormatPopulation.PopulationFormat(country.Population).PadLeft(15)}");
+            }
         }
 
         public static void WithDictionary(CsvReader reader)
